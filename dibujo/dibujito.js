b9acbebb039854539fd4a07a3f51569f58b93a58
nonamoes <chiusagui@gmail.com>
2022-02-28 13:44:24 -0600

    Termine dibujo lineas con While y FOR
    Se termino ya en color pink las lineas con un ciclo WHILE y FOR, el while esta comentado (:

--- dibujo/dibujito.js
+++ dibujo/dibujito.js
@@ -10,6 +10,37 @@ lienzo.stroke();
 lienzo.closePath(); //fin dibujo
 */
 
+var lineas =30;
+var l =0;
+var yi, xf;
+
+//Esto es con el ciclo WHILE
+/*while(l<lineas)
+{
+    yi = 10 *l;
+    xf= 10 * (l+1);
+
+    dibujarLinea("pink", 0, yi, xf, 300);
+    console.log("linea" + l);
+    l++;
+
+}*/
+
+//Esto es con el ciclo FOR
+for (l=0; l<lineas; l++)
+{
+    yi = 10 *l;
+    xf= 10 * (l+1);
+
+    dibujarLinea("pink", 0, yi, xf, 300);
+    console.log("linea" + l);
+    //l++; //si quiero quede mas espaciado
+
+}
+
+dibujarLinea("pink", 1, 1, 1, 299);  //un punto mas si inicia en 0 y un punto menos de los 300 
+dibujarLinea("pink", 1, 299, 299, 299);
+
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -21,5 +52,4 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
     lienzo.closePath(); 
 }
 
-dibujarLinea("pink",10,300,220,10);
-dibujarLinea("purple",10,300,220,200);
+
