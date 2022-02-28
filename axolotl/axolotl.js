caf8c5851609768cd995062f1e53620c99fcf7ca
nonamoes <chiusagui@gmail.com>
2022-02-28 13:10:01 -0600

    Update axolotl.js
    Crear funcion, para tomar el color, xinicial, xfinal, yinicial, yfinal

--- axolotl/axolotl.js
+++ axolotl/axolotl.js
@@ -2,11 +2,19 @@ var d= document.getElementById("dibujito");
 var lienzo = d.getContext("2d");  //Objeto canvas y se hara en 2 D
 console.log(lienzo);
 
-
-lienzo.beginPath(); //Arrancar un trazo en js
+/* lienzo.beginPath(); //Arrancar un trazo en js
 lienzo.strokeStyle="pink";  //atributo, propiedad o variable.
 lienzo.moveTo(100, 100);
 lienzo.lineTo(200, 200);
 lienzo.stroke();
-lienzo.closePath();
-
+lienzo.closePath(); //fin dibujo
+*/
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+    lienzo.beginPath(); 
+    lienzo.strokeStyle=color;  
+    lienzo.moveTo(xinicial, yinicial);
+    lienzo.lineTo(xfinal, yfinal);
+    lienzo.stroke();
+    lienzo.closePath(); 
+}
