adebc250c526aacbcb7695e7724b555769cecdbd
nonamoes <chiusagui@gmail.com>
2022-02-28 13:02:28 -0600

    1st canva linea
    Mi primer linea en canvas awh

--- axolotl/axolotl.js
+++ axolotl/axolotl.js
@@ -2,3 +2,11 @@ var d= document.getElementById("dibujito");
 var lienzo = d.getContext("2d");  //Objeto canvas y se hara en 2 D
 console.log(lienzo);
 
+
+lienzo.beginPath(); //Arrancar un trazo en js
+lienzo.strokeStyle="pink";  //atributo, propiedad o variable.
+lienzo.moveTo(100, 100);
+lienzo.lineTo(200, 200);
+lienzo.stroke();
+lienzo.closePath();
+
