a1c365a38276561c75987e95a3593f2a9156bea3
nonamoes <chiusagui@gmail.com>
2022-03-02 08:53:42 -0600

    funcion dibujoPorClick y colores
    Lista la funcion con el boton para que me pregunte cuantas lineas quiero dibujar ^^

--- dibujoform/dibujito.js
+++ dibujoform/dibujito.js
@@ -1,45 +1,13 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);  //Estamos referenciando la funcion no ejecutandola por eso no se pone el ()/que en VB es el onClick
+
+
 var d= document.getElementById("dibujito");
 var lienzo = d.getContext("2d");  //Objeto canvas y se hara en 2 D
 console.log(lienzo);
+var ancho= d.width;  //aqui elegimos el ancho para sacar las lineas
 
-/* lienzo.beginPath(); //Arrancar un trazo en js
-lienzo.strokeStyle="pink";  //atributo, propiedad o variable.
-lienzo.moveTo(100, 100);
-lienzo.lineTo(200, 200);
-lienzo.stroke();
-lienzo.closePath(); //fin dibujo
-*/
-
-var lineas =30;
-var l =0;
-var yi, xf;
-
-//Esto es con el ciclo WHILE
-/*while(l<lineas)
-{
-    yi = 10 *l;
-    xf= 10 * (l+1);
-
-    dibujarLinea("pink", 0, yi, xf, 300);
-    console.log("linea" + l);
-    l++;
-
-}*/
-
-//Esto es con el ciclo FOR
-for (l=0; l<lineas; l++)
-{
-    yi = 10 *l;
-    xf= 10 * (l+1);
-
-    dibujarLinea("pink", 0, yi, xf, 300);
-    console.log("linea" + l);
-    //l++; //si quiero quede mas espaciado
-
-}
-
-dibujarLinea("pink", 1, 1, 1, 299);  //un punto mas si inicia en 0 y un punto menos de los 300 
-dibujarLinea("pink", 1, 299, 299, 299);
 
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
@@ -53,3 +21,30 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 }
 
 
+
+function dibujoPorClick()
+{
+    
+    //alert("No me toques ahi");
+    //var xxx= parseInt(texto.value);
+    var lineas = parseInt(texto.value);
+    var l =0;
+    var yi, xf;
+    var espacio= ancho/lineas;
+
+
+    for (l=0; l<lineas; l++)
+    {
+        yi = espacio *l; 
+        xf= espacio * (l+1);
+
+        dibujarLinea("pink", 0, yi, xf, 300);
+        console.log("linea" + l);
+        //l++; //si quiero quede mas espaciado
+
+    }
+
+    dibujarLinea("pink", 1, 1, 1, 299);  //un punto mas si inicia en 0 y un punto menos de los 300 
+    dibujarLinea("pink", 1, 299, 299, 299);
+
+}
