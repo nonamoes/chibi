c42de1c2c4c5ff8d22d9222093ea03bd2b47ac09
nonamoes <chiusagui@gmail.com>
2022-03-10 12:58:25 -0600

    dibujar canvas con teclado~
    dibujar canvas con el teclado, wow :3

--- flechitas/eventos.js
+++ flechitas/eventos.js
@@ -28,27 +28,58 @@ document.addEventListener("keyup", dibujarTeclado); //cuenta cada que suelto la 
 //     }
 // }
 
+var cuadrito = document.getElementById("area_de_dibujo"); //Identificamos como areaDibujo al canvas o lienzo de HTML.
+var papel = cuadrito.getContext("2d"); //Defino el context.
+var x = 100;
+var y = 100;
+dibujarLinea("pink", x-1, y-1, x+1,y+1, papel);  // es una linea pero se visualiza como un puntito
+
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo) {
+
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.lineWidth = 3;
+    lienzo.moveTo(xinicial, yinicial);
+    lienzo.lineTo(xfinal, yfinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
+
+
 //Haciendolo con un SWITCH:
 function dibujarTeclado(evento) {
+    var colorshito="purple";
+    var movimiento = 10; //se puede hacer mas corto por ejemplo 5
+
     switch (evento.keyCode) {
         case teclas.DOWN:
+            dibujarLinea(colorshito, x, y, x, y+movimiento, papel);
+            y= y+movimiento;
             console.log("vamos pa' DOWN");
-        break;
+            break;
 
         case teclas.UP:
+            dibujarLinea(colorshito, x, y, x, y-movimiento,papel);
+            y= y-movimiento;
             console.log("vamos pa' UP");
-        break;
+            break;
 
         case teclas.LEFT:
+            dibujarLinea(colorshito, x, y, x-movimiento, y,papel);
+            x= x-movimiento;
             console.log("vamos pa' LEFT");
-        break;
+            break;
 
         case teclas.RIGHT:
+            dibujarLinea(colorshito, x, y, x+movimiento, y,papel);
+            x= x+movimiento;
             console.log("vamos pa' RIGHT");
-        break;
-    default:
-        console.log("otra tecla mijo");
-    break;
+            break;
+        default:
+            console.log("otra tecla mijo");
+            break;
 
     }
 }
